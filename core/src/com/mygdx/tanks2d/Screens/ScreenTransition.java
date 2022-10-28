0200617334af0bda02198c2106463db4af5c8cc5
SarinViktor8755 <SarinViktor8755>
2022-10-28 09:55:04 +0600

    pause game_17

---
+++ core/src/com/mygdx/tanks2d/Screens/ScreenTransition.java
@@ -0,0 +1,9 @@
+package com.mygdx.tanks2d.Screens;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public interface ScreenTransition {
+    public float getDuration ();
+    public void render (SpriteBatch batch, Texture currScreen, Texture nextScreen, float alpha);
+}
